655dbd0fa99856a4796b06383734a412fe0864d6
bllguo <bllguo@gmail.com>
2022-04-23 20:34:09 -0700

    support different vectorizers

--- 2_vectorize_data.py
+++ 2_vectorize_data.py
@@ -1,9 +1,22 @@
 import click
 import numpy as np
-from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.feature_extraction.text import CountVectorizer, TfidfVectorizer
 
         
 def get_wiki_vocab(file):
+    """
+    Build a vocabulary from Wikipedia articles.
+    
+    Parameters
+    ----------
+    file : str
+        File path to Wikipedia article text
+    
+    Returns
+    -------
+    dict
+        Dictionary of words and their frequencies
+    """
     vocab = {}
     with open(file, 'r', encoding='utf-8') as f:
         line = f.readline()
@@ -17,6 +30,19 @@ def get_wiki_vocab(file):
 
 
 def get_notes_vocab(file):
+    """
+    Build a vocabulary from MIMIC notes text.
+    
+    Parameters
+    ----------
+    file : str
+        File path to combined_dataset from 1_preprocess_mimic.py
+    
+    Returns
+    -------
+    dict
+        Dictionary of words and their frequencies
+    """
     vocab = {}
     with open(file, 'r', encoding='utf-8') as f:
         line = f.readline()
@@ -37,6 +63,19 @@ def get_notes_vocab(file):
 
 
 def docs_to_strings(documents):
+    """
+    Convert a list of lists of tokens to a list of strings.
+    
+    Parameters
+    ----------
+    documents : list
+        List of lists of tokens
+    
+    Returns
+    -------
+    list
+        List of strings
+    """
     data = []
     for doc in documents:
         text = ''
@@ -47,6 +86,21 @@ def docs_to_strings(documents):
 
 
 def get_wiki_documents(file, vocab):
+    """
+    Get a list of lists of tokens from Wikipedia articles.
+    
+    Parameters
+    ----------
+    file : str
+        File path to Wikipedia article texts
+    vocab : dict
+        Dictionary of words and their frequencies
+        
+    Returns
+    -------
+    list
+        List of lists of tokens
+    """
     documents = []
     with open(file, 'r', encoding='utf-8') as f:
         line = f.readline()
@@ -67,6 +121,21 @@ def get_wiki_documents(file, vocab):
 
 
 def get_notes(file, vocab):
+    """
+    Get a list of lists of tokens from MIMIC notes.
+    
+    Parameters
+    ----------
+    file : str
+        File path to combined_dataset from 1_preprocess_mimic.py
+    vocab : dict
+        Dictionary of words and their frequencies
+        
+    Returns
+    -------
+    list
+        List of lists of tokens
+    """
     documents = []
     with open(file, 'r', encoding='utf-8') as f:
         line = f.readline()
@@ -90,9 +159,20 @@ def get_notes(file, vocab):
     return documents
 
 
-def vectorize(data, vocab_map, output_file):
-    vect = CountVectorizer(min_df=1, vocabulary=vocab_map, binary=True)
-    embed = vect.fit_transform(data)
+def vectorize(data, output_file, vectorizer):
+    """
+    Apply a vectorizer to a list of strings.
+    
+    Parameters
+    ----------
+    data : list
+        List of strings
+    output_file : str
+        File path to save vectorized data
+    vectorizer : sklearn.feature_extraction.text.CountVectorizer or sklearn.feature_extraction.text.TfidfVectorizer
+        Vectorizer to apply
+    """
+    embed = vectorizer.fit_transform(data)
     embed = embed.A
     embed = np.array(embed, dtype=float)
     np.save(output_file, embed)
@@ -103,10 +183,12 @@ def vectorize(data, vocab_map, output_file):
 @click.option('--file_mimic', default='data/combined_dataset')
 @click.option('--output_wiki', default='data/wikivec')
 @click.option('--output_mimic', default='data/notevec')
+@click.option('--vectorizer_type', default='binary', type=click.Choice(['binary', 'count', 'tfidf']))
 def process_data(file_wiki,
                  file_mimic,
                  output_wiki,
-                 output_mimic):
+                 output_mimic,
+                 vectorizer_type):
     wiki_vocab = get_wiki_vocab(file_wiki)
     note_vocab = get_notes_vocab(file_mimic)
     
@@ -124,8 +206,15 @@ def process_data(file_wiki,
     wiki_docs = docs_to_strings(wiki_docs)
     notes = docs_to_strings(notes)
     
-    vectorize(wiki_docs, vocab_map, output_wiki)
-    vectorize(notes, vocab_map, output_mimic)
+    if vectorizer_type == 'binary':
+        vectorizer = CountVectorizer(min_df=1, vocabulary=vocab_map, binary=True)
+    elif vectorizer_type == 'count':
+        vectorizer = CountVectorizer(min_df=1, vocabulary=vocab_map)
+    elif vectorizer_type == 'tfidf':
+        vectorizer = TfidfVectorizer(min_df=1, vocabulary=vocab_map)
+    
+    vectorize(wiki_docs, output_wiki, vectorizer)
+    vectorize(notes, output_mimic, vectorizer)
 
 
 if __name__ == "__main__":
